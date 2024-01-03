b8be75debc9148e25a5e6282c6668049cf9d2fdf
nasimFoysal <nasimfoysal@gmail.com>
2024-01-04 01:57:50 +0600

    copy password to clipboard method added

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,55 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
   const [range, setRange] = useState(8);
   const [isNumberAllowed, setIsNumberAllowed] = useState(false);
   const [isSpecialCharAllowed, setIsSpecialCharAllowed] = useState(false);
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState("");
   // console.log(isNumberAllowed);
 
-const myPassword = useCallback(()=>{
-  const letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const num = '0123456789';
-  const specialChar = '@#$%^&*¢£¥';
+  const myPassword = useCallback(() => {
+    const letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const num = "0123456789";
+    const specialChar = "@#$%^&*¢£¥";
 
-  let pass = '';
+    let pass = "";
 
-  let str = letter.slice();
-  if(isNumberAllowed) {
-    str += num;
-  }
-  if(isSpecialCharAllowed) {
-    str += specialChar;
-  } 
-  if(isNumberAllowed && isSpecialCharAllowed) {
-    let strWithNum = str + num;
-    let strWithBoth = strWithNum+ specialChar;
-    str = strWithBoth;
-  }
+    let str = letter.slice();
+    if (isNumberAllowed) {
+      str += num;
+    }
+    if (isSpecialCharAllowed) {
+      str += specialChar;
+    }
+    if (isNumberAllowed && isSpecialCharAllowed) {
+      let strWithNum = str + num;
+      let strWithBoth = strWithNum + specialChar;
+      str = strWithBoth;
+    }
 
-  for(let i=0; i<range; i++){
-    const randomNum = Math.floor( Math.random()* str.length);
-    pass += str.charAt(randomNum)
+    for (let i = 0; i < range; i++) {
+      const randomNum = Math.floor(Math.random() * str.length);
+      pass += str.charAt(randomNum);
+    }
 
-  }
+    setPassword(pass);
+  }, [isNumberAllowed, isSpecialCharAllowed, range]);
 
-  setPassword(pass);
+  useEffect(() => {
+    myPassword();
+  }, [isNumberAllowed, isSpecialCharAllowed, range, setPassword, myPassword]);
 
-}, [isNumberAllowed, isSpecialCharAllowed, range])
+  const copyPassToClipboard = useCallback(() => {
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
-useEffect(()=>{
-  myPassword()
-},[isNumberAllowed, isSpecialCharAllowed, range, setPassword, myPassword])
- 
-
-return (
+  return (
     <>
       <div>Your Random Password: {password}</div>
       <div>
-        {/* <h3>Trying to read input-range value</h3> */}
+        <button onClick={copyPassToClipboard}>Copy</button>
+      </div>
+      <div>
         <p>Drag to change the number of characters of your password.</p>
         <input
           type="range"
@@ -59,12 +62,10 @@ return (
         <label htmlFor="range">Password: {range} characters</label>
       </div>
       <div>
-        {/* <h3>Trying to read if a checkbox is clicked</h3> */}
         <input
           type="checkbox"
           onClick={() => {
             setIsNumberAllowed((prev) => !prev);
-            // console.log("Number:" +isNumberAllowed);
           }}
           name=""
           id="check"
@@ -78,7 +79,6 @@ return (
           id=""
           onClick={() => {
             setIsSpecialCharAllowed((prev) => !prev);
-            // console.log("Spec:" + isSpecialCharAllowed);
           }}
         />
         <label htmlFor="special-char">Special Characters</label>
